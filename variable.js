4d266f26c1d5912896df20fbeed0a08bce8a62c8
minumsa <icycyi@naver.com>
2022-08-16 17:53:38 +0900

    코딩의 기본 operator, if, for loop

--- variable.js
+++ variable.js
@@ -25,13 +25,19 @@ console.log(globalName);
 }
 console.log(age);
 
-// 3. Constants
+// 3. Constants, r(read only)
+// use const whenever possible.
+// only use let if variable needs to change.
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// Note!
+// Immutable data types: premitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons:
 //  - security
 //  - thread safety
 //  - reduce human mistakes
-const daysInWeek = 7;
-const maxNumber = 5;
 
 // 4. Variable types
 // primitive, single item: number, string, boolean, null, undefine, symbol
