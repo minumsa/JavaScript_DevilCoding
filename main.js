346a6e4a28fa485992139f44496528ead622d1f7
유일 <83910706+minumsa@users.noreply.github.com>
2022-08-23 12:21:16 +0900

    클래스 예제와 콜백 함수 최종 정리

---
+++ main.js
@@ -0,0 +1,31 @@
+class Counter {
+  constructor(runEveryFiveTimes) {
+    this.counter = 0;
+    this.callback = runEveryFiveTimes;
+  }
+
+  increase() {
+    this.counter++;
+    console.log(this.counter);
+    if (this.counter % 5 === 0) {
+      this.callback && this.callback(this.counter);
+    }
+  }
+}
+
+function printSomething(num) {
+  console.log(`Wow! ${num}`);
+}
+
+function alertNum(num) {
+  alert(`alert! ${num}`);
+}
+
+const printCounter = new Counter(printSomething);
+const alertCounter = new Counter(alertNum);
+
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
